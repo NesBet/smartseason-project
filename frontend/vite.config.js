039da4a4ef7c3bfbfe16af5a364b08925754e7e7
NesBet <nehmz254@gmail.com>
2026-04-21 13:05:08 +0300

    Updating vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,15 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: [".trycloudflare.com"],
+    host: "0.0.0.0",
+    port: 5173,
+    allowedHosts: [".ts.net"],
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
     hmr: {
       clientPort: 443,
       protocol: "wss",
